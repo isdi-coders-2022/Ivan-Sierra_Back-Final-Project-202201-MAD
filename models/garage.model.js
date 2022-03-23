69b134e83ba21ab85bac5ed5e0a1de73602b0ab9
Ivan <canelo_1987_@hotmail.com>
2022-03-23 15:34:15 +0100

    Delete Service

--- models/garage.model.js
+++ models/garage.model.js
@@ -1,41 +1,40 @@
 import mongoose from 'mongoose';
 
 export function garageCreator(modelName = 'Garage') {
-  const garageSchema = new mongoose.Schema({
-    user: { type: String, required: true, unique: true },
-    pass: { type: String, required: true },
-    garage_name: { type: String, required: true },
-    cif_nif: { type: String, required: true },
-    email: { type: String, required: true },
-    phone: { type: Number, required: true },
-    web: { type: String, required: true },
-    address: { type: String, required: true },
-    services: {
-        ruedas : {type: Number, default : 0},
-        aceite : {type: Number, default : 0},
-        aire : {type: Number, default : 0},
-        filtros : {type: Number, default : 0},
-        amortiguadores : {type: Number, default : 0},
-        bombillas : {type: Number, default : 0},
-        discos : {type: Number, default : 0},
-        pastillas : {type: Number, default : 0}
+    const garageSchema = new mongoose.Schema({
+        user: { type: String, required: true, unique: true },
+        pass: { type: String, required: true },
+        garage_name: { type: String, required: true },
+        cif_nif: { type: String, required: true },
+        email: { type: String, required: true },
+        phone: { type: Number, required: true },
+        web: { type: String, required: true },
+        address: { type: String, required: true },
+        services: {
+            ruedas: Number,
+            aceite: Number,
+            filtros: Number,
+            amortiguadores: Number,
+            discos: Number,
+            pastillas: Number,
+            aire: Number,
+            bombillas: Number,
+        },
+    });
+
+    garageSchema.set('toJSON', {
+        transform: (document, returnedObject) => {
+            delete returnedObject.__v;
+        },
+    });
+
+    let Garage;
+    if (mongoose.default.models[modelName]) {
+        Garage = mongoose.model(modelName);
+    } else {
+        Garage = mongoose.model(modelName, garageSchema);
     }
-
-  });
-
-  garageSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-      delete returnedObject.__v;
-    },
-  });
-
-  let Garage;
-  if (mongoose.default.models[modelName]) {
-    Garage = mongoose.model(modelName);
-  } else {
-    Garage = mongoose.model(modelName, garageSchema);
-  }
-  return Garage;
+    return Garage;
 }
 
 export const Garage = garageCreator();
--- services/db.test.js
+++ services/db.test.js
@@ -1,32 +1,40 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
-import { mongoConnect, installGarages, installServices } from './db.js';
-import data from '../data/garage.data.js';
-import mongoose from 'mongoose';
+import {
+    mongoConnect,
+    mongoDisconnect,
+    installTasks,
+    installUsers,
+} from './db.js';
+import data from '../data/task.data.js';
 
 describe('given a connection with MongoDB', () => {
     afterEach(async () => {
-        await mongoose.disconnect();
+        await mongoDisconnect();
     });
 
     test('then should be possible connect to our DB ', async () => {
         const connect = await mongoConnect();
         expect(connect).toBeTruthy();
-        await expect(connect.connections[0].name).toBe('Talleres');
+        expect(connect.connections[0].name).toBe(
+            process.env.NODE_ENV === 'test'
+                ? process.env.TESTDBNAME
+                : process.env.DBNAME
+        );
     });
 
     test('then it should be created and populated', async () => {
         await mongoConnect();
         // const { Task, connection } = await taskCreator(modelName);
-        const mockGarages = data.garages;
-        const { result: garages } = await installGarages(mockGarages);
-        const mockServices = data.services.map((item, i) => {
+        const mockUsers = data.users;
+        const { result: users } = await installUsers(mockUsers);
+        const mockTasks = data.tasks.map((item, i) => {
             const index = i <= 1 ? i : 0;
-            return { ...item, responsible: garages[index]._id };
+            return { ...item, responsible: users[index]._id };
         });
-        const { result } = await installServices(mockServices);
+        const { result } = await installTasks(mockTasks);
         expect(result).toBeTruthy();
         expect(Array.isArray(result)).toBe(true);
-        expect(result.length).toBe(data.services.length);
+        expect(result.length).toBe(data.tasks.length);
     });
 });
